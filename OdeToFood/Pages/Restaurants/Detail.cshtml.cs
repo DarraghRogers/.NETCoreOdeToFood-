3b64b7c21bce100aaeaad04499e369dc7cd08d31
Darragh Rogers <darragh.rogers@ktsl.com>
2020-05-14 10:34:14 +0100

    initial commit

---
+++ OdeToFood/Pages/Restaurants/Detail.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class DetailModel : PageModel
+    {
+        private readonly IRestaurantData restaurantData;
+
+        [TempData]
+        public string Message { get; set; }
+        public Restaurant Restaurant { get; set; }
+        //using a paramater and not a property because it is strictly an input model
+
+        public DetailModel(IRestaurantData restaurantData )
+        {
+            this.restaurantData = restaurantData;
+        }
+        public IActionResult OnGet(int restaurantId)
+        {
+            //Restaurant = new Restaurant();
+            //Restaurant.Id = restaurantId;
+
+            Restaurant = restaurantData.GetById(restaurantId);
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+    }
+}
---
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
+        [BindProperty(SupportsGet =true)]
+        public string SearchTerm { get; set; }
+        public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        public ListModel(IConfiguration config, 
+                         IRestaurantData restaurantData)
+        {
+            this.config = config;
+            this.restaurantData = restaurantData;
+        }
+
+        //use the paramater for the Name of the html input property- they must match so it is found
+        public void OnGet()
+        {
+            
+            //Message = "Hello, World!";
+            Message = config["Message"];
+            Restaurants = restaurantData.GetRestaurantsByName(SearchTerm);
+        }
+    }
+}
